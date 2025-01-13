b1b27e3d8d567c0e8686d8a310e4987426e09c15
Oscar <s214735@dtu.dk>
2025-01-13 11:12:45 +0100

    more testing

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,4 +1,7 @@
-from p100.data import MyDataset
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/')))
+from p100.data import PokeDataset
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import torch
@@ -6,22 +9,29 @@ import torch
 
 def test_my_dataset():
     """Test the MyDataset class."""
-    dataset_train = MyDataset("data/processed", mode = "train", transform=transforms.ToTensor())
-    dataset_val = MyDataset("data/processed", mode = "val", transform=transforms.ToTensor())
-    dataset_test = MyDataset("data/processed", mode = "test", transform=transforms.ToTensor())
+    dataset_train = PokeDataset(mode = "train")
+    dataset_val = PokeDataset(mode = "val")
+    dataset_test = PokeDataset(mode = "test")
 
-    data, target_val, class_name = DataLoader(dataset_val, batch_size=1, shuffle=True)
-    data, target_test, class_name = DataLoader(dataset_test, batch_size=1, shuffle=True)
-    data, target_train, class_name = DataLoader(dataset_train, batch_size=1, shuffle=True)
+    train_dataloader = DataLoader(dataset_train, batch_size=1)
+    val_dataloader = DataLoader(dataset_val, batch_size=1)
+    test_dataloader = DataLoader(dataset_test, batch_size=1)
+
+    img_train, target_train, _ = next(iter(train_dataloader))
+    img_val, target_val, _ = next(iter(val_dataloader))
+    img_test, target_test, _ = next(iter(test_dataloader))
+
     # Check data class
     assert isinstance(dataset_train, Dataset)
     # Check data shape
-    assert data.shape == (1, 3, 128, 128)
+    assert img_train.shape == (1, 3, 128, 128)
+
     # Check length of dataset
-    assert len(target_train) + len(target_val) + len(target_test) == 26539
+    assert len(train_dataloader) + len(val_dataloader) + len(test_dataloader) == 26539
     # Check if all classes are represented
     target_train = torch.unique(target_train)
-    assert (target_train == torch.arange(0,999)).all()
-
+    print(dir(val_dataloader.targets))
+    #assert (target_train == torch.arange(0,999)).all()
+    
 if __name__ == "__main__":
     test_my_dataset()
