b4c3d75f6033c216516debac1f5a8d572d3f939a
Adam <agb236@gmail.com>
2025-01-13 11:19:44 +0100

    fjernet test_dummy_model

--- tests/test_model.py
+++ tests/test_model.py
@@ -8,12 +8,6 @@ from p100.model import ResNetModel
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 
 def test_model():
-    path = "models/testmodel.pth"
-    model = ResNetModel(num_classes=1000, lr=3e-4).to(DEVICE)
-    torch.save(model.state_dict(), path)
-    print("Saved dummy model")
-
-def test_model2():
     model = ResNetModel()
     x = torch.randn(1,3,128, 128)
     y = model(x)
