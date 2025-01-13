9fc61460f5a5ff1b74450fc062d650049da491ed
Oscar <s214735@dtu.dk>
2025-01-13 11:18:13 +0100

    testing

--- tests/test_data.py
+++ tests/test_data.py
@@ -5,7 +5,7 @@ from p100.data import PokeDataset
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import torch
-
+import numpy as np
 
 def test_my_dataset():
     """Test the MyDataset class."""
@@ -23,15 +23,15 @@ def test_my_dataset():
 
     # Check data class
     assert isinstance(dataset_train, Dataset)
+    
     # Check data shape
     assert img_train.shape == (1, 3, 128, 128)
 
     # Check length of dataset
     assert len(train_dataloader) + len(val_dataloader) + len(test_dataloader) == 26539
+    
     # Check if all classes are represented
-    target_train = torch.unique(target_train)
-    print(dir(val_dataloader.targets))
-    #assert (target_train == torch.arange(0,999)).all()
+    assert len(np.unique(train_dataloader.dataset.targets)) == 1000
     
 if __name__ == "__main__":
     test_my_dataset()
