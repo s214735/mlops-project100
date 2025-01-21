ba2ad011d3141d2644905869f7f762bd4bafaabb
Oscar <s214735@dtu.dk>
2025-01-21 14:48:53 +0100

    remove earlystop

--- src/p100/train.py
+++ src/p100/train.py
@@ -1,23 +1,21 @@
-from pathlib import Path
+import json
 
 import hydra
 import pytorch_lightning as pl
 import torch
+from google.cloud import secretmanager
+from model import ResNetModel
 from omegaconf import DictConfig
-from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
 import wandb
-
 from data import PokeDataset
-from model import ResNetModel
-
-import json
-from google.cloud import secretmanager
 
 BUCKET_NAME = "mlops_bucket100"
+
 
 def get_wandb_api_key() -> str:
     """
@@ -40,6 +38,7 @@ def get_wandb_api_key() -> str:
 
     return api_key
 
+
 @hydra.main(config_path="../../configs", config_name="config.yaml", version_base=None)
 def train(cfg: DictConfig):
     """
@@ -48,7 +47,7 @@ def train(cfg: DictConfig):
     Args:
         cfg (DictConfig): Configuration loaded from Hydra.
     """
-    my_key= get_wandb_api_key()
+    my_key = get_wandb_api_key()
     wandb.login(key=my_key)
     # Initialize wandb
     wandb.finish()
@@ -85,9 +84,7 @@ def train(cfg: DictConfig):
         ]
     )
 
-    train_dataset = PokeDataset(
-        BUCKET_NAME, mode="test", transform=transform_train
-    )
+    train_dataset = PokeDataset(BUCKET_NAME, mode="test", transform=transform_train)
     train_loader = DataLoader(
         train_dataset,
         batch_size=cfg.train.batch_size,
@@ -113,7 +110,7 @@ def train(cfg: DictConfig):
         dirpath="./models", monitor="val_loss", mode="min", filename="m{cfg.train.epochs:02d}-{val_loss:.2f}"
     )
 
-    early_stopping_callback = EarlyStopping(monitor="val_loss", patience=5, verbose=True, mode="min")
+    # early_stopping_callback = EarlyStopping(monitor="val_loss", patience=5, verbose=True, mode="min")
 
     # Initialize PyTorch Lightning Trainer
     trainer = pl.Trainer(
@@ -122,7 +119,7 @@ def train(cfg: DictConfig):
         devices=cfg.train.devices,
         log_every_n_steps=cfg.train.log_every_n_steps,
         logger=wandb_logger,
-        callbacks=[checkpoint_callback, early_stopping_callback],
+        callbacks=[checkpoint_callback],  # , early_stopping_callback],
     )
 
     # Train the model
